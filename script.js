0fba401045b88d34a02c7a9ea8d5a83e6d5c469c
cedrooDev <148737788+cedrooDev@users.noreply.github.com>
2023-12-08 11:02:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+hamburger = document.querySelector(".hamburger");
+
+hamburger.onclick = () => {
+    navBar = document.querySelector(".nav__bar");
+    navBar.classList.toggle("active");
+
+    
+}
